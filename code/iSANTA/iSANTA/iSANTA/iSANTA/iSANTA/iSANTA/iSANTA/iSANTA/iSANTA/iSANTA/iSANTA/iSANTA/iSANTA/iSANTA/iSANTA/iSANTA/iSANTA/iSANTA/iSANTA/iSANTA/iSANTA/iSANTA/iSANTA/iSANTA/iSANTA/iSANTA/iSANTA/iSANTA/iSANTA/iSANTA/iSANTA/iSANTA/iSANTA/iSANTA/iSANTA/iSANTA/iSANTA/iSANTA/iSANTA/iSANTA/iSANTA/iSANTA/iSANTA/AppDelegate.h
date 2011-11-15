69723261bf65e32c54d457222f0012c5d20c6576
Jack Hall <hall.d.jack@gmail.com>
2011-11-15 00:37:56 -0500

    Added the rough picker view. Still scrolling improperly...

---
+++ code/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/iSANTA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iSANTA
+//
+//  Created by Jack Hall on 10/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
