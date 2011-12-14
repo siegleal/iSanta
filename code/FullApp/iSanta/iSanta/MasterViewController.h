f0e0128a55a29e6e169f0051a82eca3f0c3b708e
Jack Hall <hall.d.jack@gmail.com>
2011-12-14 16:32:01 -0500

    Started the base of the full app. Nothing is working at this point. A Core Data file is completed.

---
+++ code/FullApp/iSanta/iSanta/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  iSanta
+//
+//  Created by Jack Hall on 12/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ code/FullApp/iSanta/iSantaTests/iSantaTests.h
@@ -0,0 +1,13 @@
+//
+//  iSantaTests.h
+//  iSantaTests
+//
+//  Created by Jack Hall on 12/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iSantaTests : SenTestCase
+
+@end
