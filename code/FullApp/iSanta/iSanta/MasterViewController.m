d6457a816a07dd66c566ed75a1264e6ec9d11d63
Andrew Siegle <siegleal@rose-hulman.edu>
2012-05-15 16:46:45 -0400

    builds opencv

--- code/FullApp/iSanta/iSanta/MasterViewController.m
+++ code/FullApp/iSanta/iSanta/MasterViewController.m
@@ -131,18 +131,6 @@
         // Delete the managed object for the given index path
         NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
         [context deleteObject:[self.fetchedResultsController objectAtIndexPath:indexPath]];
-        
-        // Save the context.
-        NSError *error = nil;
-        if (![context save:&error]) {
-            /*
-             Replace this implementation with code to handle the error appropriately.
-             
-             abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-             */
-            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-            //abort();
-        }
     }   
 }
 
